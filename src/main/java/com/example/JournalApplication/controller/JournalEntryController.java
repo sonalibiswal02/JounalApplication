3336db0040ec5a52856e608bf0e116895c01ba87
Sonali Biswal <sonalibiswal02@hotmail.co.uk>
2025-12-28 17:06:46 +0530

    Added an entity class and created a simple GET and POST API

---
+++ src/main/java/com/example/JournalApplication/controller/JournalEntryController.java
@@ -0,0 +1,28 @@
+package com.example.JournalApplication.controller;
+
+import com.example.JournalApplication.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    @GetMapping()
+    public List<JournalEntry> getAll(){
+
+        return new ArrayList<>(journalEntries.values());
+
+    }
+    @PostMapping()
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/JournalApplication/entity/JournalEntry.java
@@ -0,0 +1,33 @@
+package com.example.JournalApplication.entity;
+
+
+public class JournalEntry {
+
+    private Long id;
+    private String content;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
